470b01e5b00d006c46c7e25346182d9bbb4ef8eb
Hinkula Juha <h01270@haaga-helia.fi>
2017-04-07 08:48:39 +0300

    orm deleted

--- src/main/java/fi/haagahelia/course/TodoListApplication.java
+++ src/main/java/fi/haagahelia/course/TodoListApplication.java
@@ -23,9 +23,6 @@ public class TodoListApplication {
 	@Bean
 	public CommandLineRunner studentDemo(TodoRepository repository, UserRepository urepository) {
 		return (args) -> {			
-			//repository.save(new Todo("Go to school", "Open"));
-			//repository.save(new Todo("Eat lunch", "Completed"));
-			
 			// Create users: admin/admin user/user
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
