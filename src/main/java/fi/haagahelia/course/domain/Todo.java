c673d4b0f363c5a8c040f63d7076905bc114f3f8
Hinkula Juha <h01270@haaga-helia.fi>
2017-04-07 09:00:54 +0300

    lastmodifiedby and date added

--- src/main/java/fi/haagahelia/course/domain/Todo.java
+++ src/main/java/fi/haagahelia/course/domain/Todo.java
@@ -83,6 +83,22 @@ public class Todo {
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
 	}
+		
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
 
 	@Override
 	public String toString() {
