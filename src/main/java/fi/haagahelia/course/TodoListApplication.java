4a27816e30997fa39158448b41f619bd4fcbe01d
Hinkula Juha <h01270@haaga-helia.fi>
2017-04-10 12:48:21 +0300

    refactoring

--- src/main/java/fi/haagahelia/course/TodoListApplication.java
+++ src/main/java/fi/haagahelia/course/TodoListApplication.java
@@ -21,18 +21,13 @@ public class TodoListApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner studentDemo(TodoRepository repository, UserRepository urepository) {
+	public CommandLineRunner todoDemo(UserRepository urepository) {
 		return (args) -> {			
 			// Create users: admin/admin user/user
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
 			urepository.save(user1);
 			urepository.save(user2);
-			
-			log.info("fetch all todos");
-			for (Todo todo : repository.findAll()) {
-				log.info(todo.toString());
-			}
 		};
 	}	
 }
