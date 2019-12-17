b8a570fe98b44b78165443a54701a601bb2a4e55
juhahinkula <hinkula@gmail.com>
2019-12-17 10:59:37 +0200

    Spring Boot upgrade

--- src/main/java/fi/haagahelia/course/TodoListApplication.java
+++ src/main/java/fi/haagahelia/course/TodoListApplication.java
@@ -1,20 +1,15 @@
 package fi.haagahelia.course;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import fi.haagahelia.course.domain.Todo;
-import fi.haagahelia.course.domain.TodoRepository;
 import fi.haagahelia.course.domain.User;
 import fi.haagahelia.course.domain.UserRepository;
 
 @SpringBootApplication
 public class TodoListApplication {
-	private static final Logger log = LoggerFactory.getLogger(TodoListApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
